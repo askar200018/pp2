840c00879c095bad38ff041df3eceed5736fe5d3
askar200018 <imangali950@gmail.com>
2019-02-07 16:46:52 +0600

    week2

---
+++ week2/Task2/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        public static bool isprime(int a)
+        {
+            if (a == 1) return false;
+            for (int i = 2; i <= Math.Sqrt(a); i++)
+            {
+                if (a % i == 0) return false;
+            }
+            return true;
+        }
+        static void Prime()
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string[] arr = sr.ReadLine().Split();
+            StreamWriter sw = new StreamWriter("output.txt");
+            int[] arr2 = new int[1000];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(isprime(int.Parse(arr[i])) == true)
+                {
+                    sw.Write(int.Parse(arr[i]) + " ");
+                }
+            }
+            sr.Close();
+            sw.Close();
+        }
+        static void Main(string[] args)
+        {
+            Prime();
+        }
+    }
+}
---
+++ week2/task1/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        public static void Palindrome()
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string s = sr.ReadLine();
+            int count = 0;
+            for (int i = 0; i < s.Length / 2 ; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    count++;
+                    break;
+                }
+            }
+            if (count >= 1)
+            {
+                Console.WriteLine("No");
+            }
+            else Console.WriteLine("Yes");
+            sr.Close();
+        } 
+        static void Main(string[] args)
+        {
+            Palindrome();
+        }
+    }
+}
