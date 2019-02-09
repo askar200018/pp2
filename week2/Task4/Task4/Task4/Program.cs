9d8985e220eb423dfb7058c76e3614cc1fc8a6af
askar200018 <imangali950@gmail.com>
2019-02-09 19:17:08 +0600

    add task3 and task 4 in lab2

---
+++ week2/Task4/Task4/Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath1 = @"C:\Users\Аскар\Documents\pp2_Aks\path\output1.txt";
+            string FilePath2 = @"C:\Users\Аскар\Documents\pp2_Aks\path\output2.txt";
+            StreamWriter sw = new StreamWriter(FilePath1);
+            sw.WriteLine("random string");
+            sw.Close();
+            File.Copy(FilePath1, FilePath2);
+            File.Delete(FilePath1);
+
+        }
+    }
+}
